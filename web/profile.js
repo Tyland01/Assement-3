eddeb90499a7771bf966e25d8cf21822e7220485
Tyland Sheard <77373702+Tyland01@users.noreply.github.com>
2021-09-17 13:29:10 -0400

    Add files via upload

---
+++ web/profile.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("#color");
+
+button.addEventListener("click", () => {
+  alert("color");
+});
+
+const button = document.querySelector("#place");
+
+button.addEventListener("click", () => {
+  alert("place");
+});
+
+const button = document.querySelector("#ritual");
+
+button.addEventListener("click", () => {
+  alert("ritual");
+});
